e7ac27ca59a00f4bac391d259cc971f13d2baaaf
Michał Stańczyk <michal.stanczyk@itmagination.com>
2023-04-28 08:27:55 +0200

    Setup commit

---
+++ playwright.config.ts
@@ -0,0 +1,29 @@
+import type { PlaywrightTestConfig } from '@playwright/test'
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+const config: PlaywrightTestConfig = {
+  testDir: './tests',
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  reporter: [['html', { open: 'never' }]],
+  use: {
+    browserName: 'chromium',
+    headless: true,
+    screenshot: 'only-on-failure',
+    trace: 'retain-on-failure',
+    baseURL: 'https://automationteststore.com/',
+  },
+}
+
+export default config
